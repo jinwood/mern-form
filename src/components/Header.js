5474bc2b2fea1b35ec325e5aa2f767ab45d49dbb
jinwood <jinw@protonmail.com>
2021-03-24 21:15:21 +0000

    First step

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Progress from "./Progress";
 
 const Header = () => {
   return (
     <div>
       <h1>Multi Step Registration</h1>
+      <Progress />
     </div>
   );
 };
