c0e7721a47f9fd56d0c6d81273d2eae8e77ed755
jinwood <jinw@protonmail.com>
2021-03-24 20:19:58 +0000

    init

---
+++ src/components/FirstStep.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirstStep = () => {
+  return <div>First step</div>;
+};
+
+export default FirstStep;
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FirstStep from "../components/FirstStep";
+import Header from "../components/Header";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route component={FirstStep} path="/" exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
